3f46bfd0c86ba6317b119cbb99658b5a5954b17e
Seungwan Han <sehan@c2r9s6.42seoul.kr>
2022-09-02 10:17:36 +0900

    [Feat] login 기능 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Routes/Login/Login";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-[#f6f6f6] flex justify-center">
-      <div className="w-96">
-        <Login />
+    <Router>
+      <div className="w-screen h-screen bg-[#f6f6f6] flex justify-center">
+        <div className="w-96">
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/Routes/Login/Login.tsx
+++ frontend/src/Routes/Login/Login.tsx
@@ -5,9 +5,18 @@ function Login() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <h1 className="h-40 text-4xl">부동산 체크리스트</h1>
-      <a className="w-10/12" href={process.env.REACT_APP_API_URL}>
-        <Button text="카카오 로그인" />
-      </a>
+      <div className="w-full p-2">아이디</div>
+      <input className="w-full p-2"></input>
+      <div className="w-full p-2">비밀번호</div>
+      <input type="password" className="w-full p-2"></input>
+      <div className="flex w-full pt-6">
+        <a className="w-full p-2">
+          <Button text="로그인" />
+        </a>
+        <a className="w-full p-2">
+          <Button text="회원가입" />
+        </a>
+      </div>
     </div>
   );
 }
