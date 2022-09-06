e362546f71e86a884a229b4095cee64b1f58d3f7
한승완 <hanseung-wan@hanseung-wan-ui-MacBookPro.local>
2022-09-06 02:38:29 -0700

    [Feat] 버튼 추가
    - houseType -> type 으로 변수 이름 변경
    - 중요도 수정 버튼, 추가 버튼 추가

--- frontend/src/Routes/House/HouseComponent.tsx
+++ frontend/src/Routes/House/HouseComponent.tsx
@@ -7,8 +7,9 @@ type PropsType = {
 };
 
 function HouseComponent({ house }: PropsType) {
+  console.log("house");
   const money = () => {
-    if (house.houseType === HouseEnum.DEALING) {
+    if (house.type === HouseEnum.DEALING) {
       return house.price;
     } else {
       return `${house.deposit}/${house.rent}/${house.maintenanceFee}`;
@@ -16,13 +17,13 @@ function HouseComponent({ house }: PropsType) {
   };
 
   const typeText = () => {
-    if (house.houseType === HouseEnum.DEALING) return "매매";
-    else if (house.houseType === HouseEnum.JEONSE) return "전세";
+    if (house.type === HouseEnum.DEALING) return "매매";
+    else if (house.type === HouseEnum.JEONSE) return "전세";
     else return "월세";
   };
 
   return (
-    <Link to={"/House/1"}>
+    <Link to={"/house/1"}>
       <div className="flex w-full justify-center">
         <p className="pl-2 pr-2 text-xl">{typeText()}</p>
         <p className="pl-2 pr-2 text-xl">{house.title}</p>
--- frontend/src/Routes/House/HouseList.tsx
+++ frontend/src/Routes/House/HouseList.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import HouseComponent from "./HouseComponent";
 import axios from "axios";
 import { getConfig } from "../../function/getConfig";
+import Button from "../../Components/Button";
+import { Link } from "react-router-dom";
 
 export enum HouseEnum {
   DEALING,
@@ -12,7 +14,7 @@ export enum HouseEnum {
 export type HouseType = {
   id: number;
   title: string;
-  houseType: HouseEnum;
+  type: HouseEnum;
   area: number;
   price: number;
   deposit: number;
@@ -21,12 +23,11 @@ export type HouseType = {
 };
 
 function HouseList() {
-  const [houses, setHouses] = useState<HouseType[]>();
+  const [houses, setHouses] = useState<HouseType[]>([]);
   useEffect(() => {
     axios
       .get(process.env.REACT_APP_API_URL + "house", getConfig())
       .then((res) => {
-        console.log(res);
         setHouses(res.data);
       })
       .catch();
@@ -35,11 +36,15 @@ function HouseList() {
   return (
     <div className="w-full">
       <h1 className="text-4xl text-center">리스트</h1>
-      {houses
-        ? houses.map((house) => {
-            <HouseComponent house={house} />;
-          })
-        : null}
+      {houses.map((house) => {
+        return <HouseComponent key="0" house={house} />;
+      })}
+      <Link to="/importance">
+        <Button text="중요도 수정" />
+      </Link>
+      <Link to="/house/0">
+        <Button text="추가" />
+      </Link>
     </div>
   );
 }
