5c145bb43e46704c127da9f72c78dbe890bea8f8
Yejin Song <yejsong@c2r9s4.42seoul.kr>
2022-09-16 16:56:07 +0900

    [Feat] logout button 추가

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { customAxios } from "../function/customAxios";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Cookies } from "react-cookie";
 
 type UserType = {
   id: number;
@@ -10,17 +12,43 @@ type UserType = {
 
 function Header() {
   const [user, setUser] = useState<UserType>();
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+
+  const onClick = () => {
+    axios
+      .post(
+        process.env.REACT_APP_API_URL + "auth/logout",
+        {},
+        {
+          headers: {
+            Authorization: "Bearer " + cookies.get("refreshToken"),
+          },
+        },
+      )
+      .then(() => {
+        cookies.remove("accessToken");
+        cookies.remove("refreshToken");
+
+        navigate("/");
+      });
+  };
+
   useEffect(() => {
     customAxios.get("user").then((res) => {
       setUser(res.data);
     });
   }, []);
+
   return (
     <div>
       {user ? (
-        <Link to="/importance">
-          <p className="text-right">{user.nickname}</p>
-        </Link>
+        <div className="text-right">
+          <Link to="/importance">
+            <button>{user.nickname}</button>
+          </Link>
+          <button onClick={onClick}>| logout</button>
+        </div>
       ) : null}
     </div>
   );
