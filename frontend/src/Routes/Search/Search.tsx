7245600848687cad433cf39bc3b7ccd144151f4f
songtofu <songtofu0502@gmail.com>
2022-09-27 23:41:56 +0900

    [Fix] search List

--- frontend/src/Routes/Search/Search.tsx
+++ frontend/src/Routes/Search/Search.tsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import Footer from "../../Components/Footer";
 import { customAxios } from "../../function/customAxios";
 import HouseOfferingComponent from "../HouseOffering/HouseOfferingComponent";
 import { HouseOfferingType } from "../HouseOffering/HouseOfferingList";
 
 function Search() {
-  const { address } = useParams();
-  const [houseOfferings, setHouseOfferings] = useState<HouseOfferingType[]>([]);
-  const [searchAddress, setSearchAddress] = useState("");
+  const [houseOfferings, setHouseOfferings] = useState<HouseOfferingType[]>();
+  const [address, setAddress] = useState("");
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchAddress(e.target.value);
+    setAddress(e.target.value);
   };
 
   const onEnter = (e) => {
@@ -19,6 +17,7 @@ function Search() {
       customAxios.get(`/house/search/${address}`).then((res) => {
         setHouseOfferings(res.data);
       });
+      console.log(houseOfferings);
     }
   };
 
@@ -27,28 +26,30 @@ function Search() {
       setHouseOfferings(res.data);
     });
   }, []);
-
+  console.log("asdf", houseOfferings);
   return (
-    <div className="w-full">
-      <div className="flex flex-col w-full items-center justify-center pb-10">
-        <div className="flex justify-between">
-          <img className="left-0 w-5" src="/img/black_search.png" alt="" />
-          <input
-            onChange={onChangeSearch}
-            value={address}
-            placeholder="(동)으로 검색"
-            onKeyPress={onEnter}
-          />
-        </div>
-        {houseOfferings.map((houseOffering) => {
-          <HouseOfferingComponent
-            key={houseOffering.id}
-            houseOffering={houseOffering}
-            setHouseOfferings={setHouseOfferings}
-          />;
-        })}
-        <Footer />
+    <div className="flex flex-col w-full items-center pb-10">
+      <div className="flex justify-between">
+        <img className="left-0 w-5" src="/img/black_search.png" alt="" />
+        <input
+          onChange={onChangeSearch}
+          value={address}
+          placeholder="(동)으로 검색"
+          onKeyPress={onEnter}
+        />
       </div>
+      {houseOfferings
+        ? houseOfferings.map((houseOffering) => {
+            return (
+              <HouseOfferingComponent
+                key={houseOffering.id}
+                houseOffering={houseOffering}
+                setHouseOfferings={setHouseOfferings}
+              />
+            );
+          })
+        : null}
+      <Footer />
     </div>
   );
 }
